be89b24ef67e4ed5477ebe6fefee47a91fbd66a9
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-25 19:53:42 +0700

    ViewModelScoped

--- app/src/main/java/com/hoc081098/paginationmviflow/ui/main/MainModule.kt
+++ app/src/main/java/com/hoc081098/paginationmviflow/ui/main/MainModule.kt
@@ -4,10 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 interface MainModule {
   @Binds
+  @ViewModelScoped
   fun provideMainInteractor(impl: MainInteractorImpl): MainContract.Interactor
 }
